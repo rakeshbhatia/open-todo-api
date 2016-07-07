7782a375aa5eb3394b21c8e7d0b73710c4a6af05
Rakesh <rakeshbhatia87@gmail.com>
2016-07-07 10:20:22 -0700

    Checkpoint 5 destroy lists: completed

--- db/migrate/20160704024927_create_items.rb
+++ db/migrate/20160704024927_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :description
-
+      t.boolean :completed, null: false, default: false
       t.timestamps null: false
     end
   end
